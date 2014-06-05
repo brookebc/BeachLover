2bdbb0dad3df81cc2777bfec0e709f1bf1f48193
brookebc <brooke.casey@gmail.com>
2014-06-05 11:20:42 -0400

    updated requirements document a little

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,11 @@ angular.module('beachLoverApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/beachdetail', {
+        authRequired: false, // if true, must log in before viewing this page
+        templateUrl: 'views/detail.html',
+        controller: 'MainCtrl'
+      })
       .when('/login', {
         authRequired: false, // if true, must log in before viewing this page
         templateUrl: 'views/login.html',
